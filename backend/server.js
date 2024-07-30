03bb87108ec2d8e076b23727c133522d25944258
Satendra Mishra <msatendra424@gmail.com>
2024-07-30 23:24:25 +0530

    signing up and hashing passwords

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 // const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 require('dotenv').config()
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express();
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // database connection
 mongoose.connect(process.env.MONGO_URI)
