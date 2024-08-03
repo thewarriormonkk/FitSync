7f16213063b5452f8027334347e1f927e112787d
Satendra Mishra <thewarriormonkk@users.noreply.github.com>
2024-08-03 17:56:07 +0530

    useLogin & useLogout hooks

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,38 +1,38 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 
-export const useSignup = () => {
+export const useLogin = () => {
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(null)
     const { dispatch } = useAuthContext()
 
-    const signup = async (email, password) => {
+    const login = async (email, password) => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/signup', {
+        const response = await fetch('/api/user/login', {
             method: 'POST',
-            headers: { 'Content-type': 'application/json' },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
         })
-
         const json = await response.json()
 
         if (!response.ok) {
             setIsLoading(false)
             setError(json.error)
         }
+
         if (response.ok) {
-            // save the user to local storage
+            // the the user to the local storage
             localStorage.setItem('user', JSON.stringify(json))
 
             // update the auth context
             dispatch({ type: 'LOGIN', payload: json })
 
-            // update the auth context
             setIsLoading(false)
         }
     }
 
-    return { signup, isLoading, error }
+    return { login, isLoading, error }
+
 }
