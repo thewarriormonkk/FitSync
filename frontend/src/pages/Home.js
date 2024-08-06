2a3c189c9ec1d60174a735f267f2555c2cdbf1af
Satendra Mishra <thewarriormonkk@users.noreply.github.com>
2024-08-06 16:09:48 +0530

    making authorized requests

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // components
 import WorkoutDetail from '../components/WorkoutDetail'
@@ -7,19 +8,26 @@ import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
     const { workouts, dispatch } = useWorkoutsContext()
+    const { user } = useAuthContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts')
+            const response = await fetch('/api/workouts', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
             const json = await response.json()
-            console.log(json)
 
             if (response.ok) {
                 dispatch({ type: 'SET_WORKOUTS', payload: json.workouts })
             }
         }
-        fetchWorkouts()
-    }, [dispatch])
+
+        if (user) {
+            fetchWorkouts()
+        }
+    }, [dispatch, user])
 
     return (
         <div className="home">
