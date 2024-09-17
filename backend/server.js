65745666a005783bbe95db4cfba577b4e08e0f39
Satendra Mishra <thewarriormonkk@users.noreply.github.com>
2024-09-17 15:05:09 +0530

    added dotenv package

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for incoming requests
-        app.listen(process.env.PORT, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log(`connected to db & listening on port ${process.env.PORT} `)
         });
     }).catch((err) => {
